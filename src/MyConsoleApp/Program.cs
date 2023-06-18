9f4f8b2d4b4cf99aea31bc18877671a8faef8b7e
LiteObject <Lite.Object@gmail.com>
2023-06-18 11:05:49 -0500

    minor changes

--- src/MyConsoleApp/Program.cs
+++ src/MyConsoleApp/Program.cs
@@ -2,20 +2,26 @@
 // Configure the builder with your cluster's contact points
 using Cassandra;
 
-Cluster cluster = Cluster.Builder()
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Cluster cluster = Cluster.Builder()
                      .AddContactPoints("localhost")
                      .WithPort(9042)
                      .Build();
 
-// Connect to the nodes using a keyspace
-ISession session = cluster.Connect();
+        // Connect to the nodes using a keyspace
+        ISession session = cluster.Connect();
 
-// Execute a query on a connection synchronously
-RowSet rs = session.Execute("SELECT * FROM system_schema.tables;");
+        // Execute a query on a connection synchronously
+        RowSet rs = session.Execute("SELECT * FROM system_schema.tables;");
 
-// Iterate through the RowSet
-foreach (Row? row in rs)
-{
-    string name = row.GetValue<string>("keyspace_name");
-    Console.WriteLine(name);
+        // Iterate through the RowSet and print the results
+        foreach (Row? row in rs)
+        {
+            string name = row.GetValue<string>("keyspace_name");
+            Console.WriteLine(name);
+        }
+    }
 }
